17098199677cf8a97fb246f51adba2ebe405a6f6
Nathan King <34531378+nathan-king@users.noreply.github.com>
2020-05-20 16:25:18 +1000

    add my signature

--- components/Footer.js
+++ components/Footer.js
@@ -10,14 +10,16 @@ export default function Footer() {
     return (
         <footer>
             <div className={styles.social}>
-                <a href="https://twitter.com/minimalmonkey" className={styles.iconButton}><FontAwesomeIcon icon={faTwitter} className={styles.icon} id={styles.twitterIcon} /><span className={styles.twitter}></span></a>
-                <a href="https://facebook.com" className={styles.iconButton}><FontAwesomeIcon icon={faFacebookF} className={styles.icon} id={styles.facebookIcon} /><span className={styles.facebook}></span></a>
-                <a href="https://plus.google.com" className={styles.iconButton}><FontAwesomeIcon icon={faInstagram} className={styles.icon} id={styles.instagramIcon} /><span className={styles.instagram}></span></a>
+                <a href="https://twitter.com/sydneysauna" className={styles.iconButton}><FontAwesomeIcon icon={faTwitter} className={styles.icon} id={styles.twitterIcon} /><span className={styles.twitter}></span></a>
+                <a href="https://www.facebook.com/sydneysauna/" className={styles.iconButton}><FontAwesomeIcon icon={faFacebookF} className={styles.icon} id={styles.facebookIcon} /><span className={styles.facebook}></span></a>
+                <a href="https://www.instagram.com/sydneysaunaofficial/" className={styles.iconButton}><FontAwesomeIcon icon={faInstagram} className={styles.icon} id={styles.instagramIcon} /><span className={styles.instagram}></span></a>
             </div>
             <div className={styles.footer}>
                 <p>Sydney Sauna, Level 1, 38-42 Oxford Street,</p> 
                 <p>Darlinghurst, NSW, 2010, Australia | (02) 9360 3431 | info@sydneysauna.com</p>
                 <p class="font-weight-light">Copyright © 2020 Sydney Sauna</p>
+                <br />
+                <p>Created by <a className={styles.link} href="https://github.com/nathan-king">Nathan King (Web Developer)</a></p>
             </div>
         </footer>
     )
