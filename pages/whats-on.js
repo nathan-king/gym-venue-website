134c94a882ad3608fce488bfca48a71aef36ef1a
Nathan King <34531378+nathan-king@users.noreply.github.com>
2020-05-20 16:07:56 +1000

    responsive

--- pages/whats-on.js
+++ pages/whats-on.js
@@ -14,14 +14,14 @@ export default function WhatsOn() {
         </Head>
   
         <Layout>
-          <Jumbotron className={styles.jumbotron}>
+          <div className={styles.jumbotron}>
             <h1 className={styles.heading}>Need to add events dynamically<br /> through a database.</h1>
 
             <p>Sydney Sauna is Sydney's premiere Gay men's sauna open 24 hours a day, 7 days a week, no matter what you're up to, or how your night is going, we are your destination. Sydney Sauna has many features available to it's customers.</p> 
             <p>From 23 serviced rooms, 2 levels, plenty of cruise areas, large cinema room, fully serviced cafe and wet area including spa, sauna & steam room.</p>
 
             <p>For more detailed explanations and information Please wander through our site, if you have any other questions please don't hesitate to contact our friendly staff.</p>
-          </Jumbotron>
+          </div>
         </Layout>
       </div>
     )
