084c95d74546b189e35f95437f05223a656a9c9c
Nathan King <34531378+nathan-king@users.noreply.github.com>
2020-05-21 14:46:24 +1000

    iframe adjustment

--- components/Map.js
+++ components/Map.js
@@ -17,16 +17,8 @@ class Map extends Component {
   render() {
     return (
         <div className={styles.container}>
-            <LoadScript googleMapsApiKey="AIzaSyBpbaNt13bc6tVBdxLQwDxJ1zgft7OOpiM">
-                <GoogleMap
-                mapContainerStyle={this.containerStyle}
-                center={this.center}
-                zoom={17}
-                className={styles.map}
-                >
-                <Marker position={{ lat: -33.877713, lng: 151.213800 }} />
-                </GoogleMap>
-            </LoadScript>
+
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13249.638419550887!2d151.20215903403985!3d-33.87910329045248!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6b12ae170528f677%3A0x5783791f7ea2aa90!2sSydney%20Sauna!5e0!3m2!1sen!2sau!4v1590035516958!5m2!1sen!2sau" width="100%" height="100%" frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0" className={styles.map}></iframe>
         </div>
     )
   }
