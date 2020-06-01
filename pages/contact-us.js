97126e789478bc6bfa5ec78672d92a7ef4f7713a
Nathan King <34531378+nathan-king@users.noreply.github.com>
2020-06-01 12:06:34 +1000

    Add automatic email to form

--- pages/contact-us.js
+++ pages/contact-us.js
@@ -1,64 +1,106 @@
-import Head from 'next/head'
-import Layout from '../components/Layout'
-import Favicon from '../components/Favicon'
-import styles from './styles/contact-us.module.scss'
-import { Button, Form, FormGroup, Label, Input, FormText, Jumbotron } from 'reactstrap';
-
+import { useState } from "react";
+import Head from "next/head";
+import Layout from "../components/Layout";
+import Favicon from "../components/Favicon";
+import styles from "./styles/contact-us.module.scss";
+import {
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  FormText,
+  Jumbotron,
+} from "reactstrap";
+import emailjs from "emailjs-com";
 
 export default function Contact() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [enquiryType, setEnquiryType] = useState("General Enquiry");
+  const [message, setMessage] = useState("");
 
-    return (
-        <div className={styles.main}>
-            <Head>
-                <title>Sydney Sauna | Contact Us</title>
-                <Favicon />
-            </Head>
-            <Layout>
+  const state = {
+    name,
+    email,
+    enquiryType,
+    message,
+  };
 
-            <Form className={styles.form}>
-                <FormGroup className={styles.formGroup}>
-                    <Label for="name">Name</Label>
-                    <Input type="text" name="name" id="name" placeholder="Name" />
-                </FormGroup>
-                <FormGroup className={styles.formGroup}>
-                    <Label for="email">Email</Label>
-                    <Input type="email" name="email" id="email" placeholder="Email" />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="enquiryType">Subject of Enquiry</Label>
-                    <Input type="select" name="select" id="enquiryType">
-                        <option>General Enquiry</option>
-                        <option>Lost Item</option>
-                        <option>Job Application</option>
-                    </Input>
-                </FormGroup>
-                <FormGroup className={styles.formGroup}>
-                    <Label for="text">Message</Label>
-                    <Input type="textarea" name="text" id="text" rows="5"/>
-                </FormGroup>
-                <FormGroup>
-                    <Label for="cv">CV/Resume (Job applications only)</Label>
-                    <Input type="file" name="file" id="cv" />
-                </FormGroup>
-                <div className={styles.buttonContainer}>
-                    <Button color="danger">Submit</Button>
-                </div>
-            </Form>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(state);
+    var service_id = "default_service";
+    var template_id = "message_template";
+    var user_id = "user_GoXzo0iR6wJ3KqB1148aq";
+    emailjs.send(service_id, template_id, state, user_id);
+  };
 
-                {/* <Jumbotron className={styles.content}>
+  return (
+    <div className={styles.main}>
+      <Head>
+        <title>Sydney Sauna | Contact Us</title>
+        <Favicon />
+      </Head>
+      <Layout>
+        <Form className={styles.form} onSubmit={handleSubmit}>
+          <FormGroup className={styles.formGroup}>
+            <Label for="name">Name</Label>
+            <Input
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Enter name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </FormGroup>
 
+          <FormGroup className={styles.formGroup}>
+            <Label for="email">Email</Label>
+            <Input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </FormGroup>
 
-                    <submit>
-                        <div  className={styles.submitField}>
-                            <h2>Contact Us</h2>
-                            <input type="text" placeholder="Name" className={styles.input} />
-                            <input type="email" placeholder="Email" className={styles.input} />
-                            <textarea placeholder="Message" className={styles.input} rows="5"></textarea>
-                            <Button color="danger" className={styles.button}>Submit</Button>
-                        </div>
-                    </submit>
-                </Jumbotron> */}
-            </Layout>
-        </div>
-    )
+          <FormGroup>
+            <Label for="enquiryType">Subject of Enquiry</Label>
+            <Input
+              type="select"
+              name="enquiryType"
+              id="enquiryType"
+              value={enquiryType}
+              onChange={(e) => setEnquiryType(e.target.value)}
+            >
+              <option value="General Enquiry">General Enquiry</option>
+              <option value="Lost Item">Lost Item</option>
+            </Input>
+          </FormGroup>
+
+          <FormGroup className={styles.formGroup}>
+            <Label for="text">Message</Label>
+            <Input
+              type="textarea"
+              name="message"
+              id="text"
+              rows="5"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            />
+          </FormGroup>
+
+          <div className={styles.buttonContainer}>
+            <Button color="danger" type="submit">
+              Submit
+            </Button>
+          </div>
+        </Form>
+      </Layout>
+    </div>
+  );
 }
